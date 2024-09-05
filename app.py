f87b799faa780e0a0f25f0fc10e0d0fdd69a4aac
CarolinaCampi <carolinalcampi@gmail.com>
2024-09-05 15:16:25 +0100

    Initial setup
    Create simple index.html and layout.html
    Create styles.css
    Create meal_planner.db
    Create favicon.ico
    Import flask-session

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 
+# Configure application
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template("index.html")
